d515010457c787256d435176995cf0a2c0f5dc75
navduhan <duhan27dec@gmail.com>
2023-09-29 19:53:38 -0600

    update

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 const mongoose =require('mongoose')
 
 mongoose.connect("mongodb://127.0.0.1:27017/default")
+mongoose.set('strictQuery', true)
 .then(console.log("Database connected"))
 .catch(e => {
     console.error('Connection error', e.message)
