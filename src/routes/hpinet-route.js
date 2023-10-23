d564b359e68c1c619fd0bee54c4ff11194d23051
navduhan <duhan27dec@gmail.com>
2023-10-23 15:18:57 -0600

    update

--- src/routes/hpinet-route.js
+++ src/routes/hpinet-route.js
@@ -207,18 +207,18 @@ router.route('/domain_results/').post(async(req,res) =>{
       console.log("i am here")
       final = await Results.find({'intdb':{'$in':body.intdb}}).limit(limit).skip(skip).exec()
       // console.log(final)
-     
+      host_protein = await Results.distinct("Host_Protein", {'intdb':{'$in':body.intdb}})
+      pathogen_protein =await Results.distinct('Pathogen_Protein', {'intdb':{'$in':body.intdb}})
       counts = await Results.find({'intdb':{'$in':body.intdb}}).count()
       console.log(`"I am here" counts`)
       // // counts = data.keys(data.shareInfo[i]).length
-      // host_protein = await Results.distinct("Host_Protein", {'intdb':{'$in':body.intdb}})
-      // pathogen_protein =await Results.distinct('Pathogen_Protein', {'intdb':{'$in':body.intdb}})
+
       // console.log(host_protein.length)
       // res.json({'results':final,'total':counts,'hostcount':host_protein.length,'pathogencount':pathogen_protein.length})
     }
     
     
-    res.json({'results':final,'total':counts})
+    res.json({'results':final,'total':counts,'hostcount':host_protein.length,'pathogencount':pathogen_protein.length})
 
 })
 router.route('/network/').get(async(req,res) =>{
