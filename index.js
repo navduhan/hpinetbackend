0536c05360f2c9e7e995a84762ffdf4ac78e4676
navduhan <duhan27dec@gmail.com>
2023-10-23 19:18:50 -0600

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const accessControl = (req,  res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   */
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3810');
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3810');
   res.header('Access-Control-Allow-Origin', 'https://bioinfo.usu.edu');
   res.header('Access-Control-Allow-Origin', 'https://kaabil.net');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
