fdd421e0ae25a5efe2e7265e54c6c9a720bbff99
navduhan <duhan27dec@gmail.com>
2023-10-24 00:22:50 -0600

    update

--- src/routes/hpinet-route.js
+++ src/routes/hpinet-route.js
@@ -207,14 +207,14 @@ router.route('/domain_results/').post(async(req,res) =>{
       console.log("i am here")
       final = await Results.find({'intdb':{'$in':body.intdb}}).limit(limit).skip(skip).exec()
       // console.log(final)
-      host_protein = await Results.distinct("Host_Protein",{'intdb':{'$in':body.intdb}})
-      pathogen_protein =await Results.distinct("Pathogen_Protein", {'intdb':{'$in':body.intdb}})
+      // host_protein = await Results.distinct("Host_Protein",{'intdb':{'$in':body.intdb}})
+      // pathogen_protein =await Results.distinct("Pathogen_Protein", {'intdb':{'$in':body.intdb}})
       counts = await Results.find({'intdb':{'$in':body.intdb}}).count()
       console.log(`"I am here" counts`)
       // // counts = data.keys(data.shareInfo[i]).length
 
-      console.log(pathogen_protein.length)
-      console.log(host_protein.length)
+      // console.log(pathogen_protein.length)
+      // console.log(host_protein.length)
       // res.json({'results':final,'total':counts,'hostcount':host_protein.length,'pathogencount':pathogen_protein.length})
     }
     
