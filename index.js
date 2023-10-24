065ae306d47d5e8b5608076452aed05b0724eb79
navduhan <duhan27dec@gmail.com>
2023-10-24 00:20:40 -0600

    update

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express()
 const apiPort = 3815
 const routes =require('./src/routes/hpinet-route')
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.use(express.urlencoded({extended:true}))
 app.use(cors("*"))
