af62d4dba29e067e8aa6f2b7980b58e22ba07798
navduhan <duhan27dec@gmail.com>
2023-10-25 15:26:36 -0600

    update

--- src/gosemsim/goPPI.js
+++ src/gosemsim/goPPI.js
@@ -8,7 +8,31 @@ const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, path
   console.log(host_genes2);
 
   // Initialize variables
-  let output = '';
+  let output;
+
+
+  // Log the command that will be executed
+  console.log(
+    "/opt/miniconda3/envs/ml-gpu/bin/python3",
+    [
+      "/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py",
+      "--hgenes",
+      host_genes2,
+      "--pgenes",
+      pathogen_genes,
+      "--host",
+      hspecies,
+      "--pathogen",
+      pspecies,
+      "--method",
+      method,
+      "--score",
+      score,
+      "--t",
+      threshold
+    ].join(" ") // Combine command arguments into a string
+  );
+
   const commandArgs = [
     "/opt/miniconda3/envs/ml-gpu/bin/python3",
     "/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py",
@@ -23,12 +47,13 @@ const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, path
 
   // Spawn a Python process to run the script
   const getS = spawn(commandArgs[0], commandArgs.slice(1));
-
   // Handle stdout data
   getS.stdout.on('data', (data) => {
-    output += data.toString();
-    console.log('output was generated: ' + data.toString());
+    output = data.toString();
+    console.log('output was generated: ' + output);
   });
+
+  getS.stdin.setEncoding = 'utf-8';
 
   // Handle stderr data
   getS.stderr.on('data', (data) => {
@@ -36,14 +61,12 @@ const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, path
   });
 
   return new Promise((resolve, reject) => {
-    const checkOutput = () => {
-      if (!output.startsWith('hpinet')) {
-        resolve(output);
-      } else {
-        setTimeout(checkOutput, 100000); // Check again after a short delay
-      }
-    };
-    checkOutput();
+    getS.stdout.on('end', async function (code) {
+
+        const resultData = output;
+            
+            resolve(resultData);
+    });
   });
 };
 
