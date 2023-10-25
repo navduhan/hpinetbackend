9013808c45cd9e9e146662181998d62287170e47
navduhan <duhan27dec@gmail.com>
2023-10-25 11:53:55 -0600

    update

--- src/introlog/introlog.js
+++ src/introlog/introlog.js
@@ -1,43 +1,56 @@
 const { spawn } = require('child_process');
 
-const path = require('path');
-const getPPI = (method, hspecies, pspecies,identity, coverage, evalue, pi, pc, pe, intdb,domdb, genes, idt)=>{
-let output;
-let getS;
-console.log("/opt/web/hpinetdb/hpinetbackend/src/introlog/hpinterolog.py", "--method", method, "--blastdb", "/home/dock_user/hpinetblast.db", "--ppidb", "ppidb", "--host_table", hspecies.toLowerCase(), "--pathogen_table", pspecies, "--host_identity", parseInt(identity), "--host_coverage", parseInt(coverage) ,"--host_evalue", parseFloat(evalue), "--pathogen_identity", parseInt(pi) ,"--pathogen_coverage", parseInt(pc) ,"--pathogen_evalue", parseFloat(pe) ,"--ppitables", intdb, '--id', idt, '--genes', genes)
-if (genes.length >0){
-    getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', ["/home/dock_user/web/hpinetdb/hpinetbackend/src/introlog/hpinterolog.py", "--method", method, "--blastdb", "/home/dock_user/hpinetblast.db", "--ppidb", "ppidb", "--host_table", hspecies.toLowerCase(), "--pathogen_table", pspecies, "--host_identity", parseInt(identity), "--host_coverage", parseInt(coverage) ,"--host_evalue", parseFloat(evalue), "--pathogen_identity", parseInt(pi) ,"--pathogen_coverage", parseInt(pc) ,"--pathogen_evalue", parseFloat(pe) ,"--ppitables", intdb, '--domdb', domdb, '--id', idt, '--genes', genes]);
+// Function to run the hpinterolog.py script
+const getPPI = async (method, hspecies, pspecies, identity, coverage, evalue, pi, pc, pe, intdb, domdb, genes, idt) => {
+  let output;
 
-}
-else{
-getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', ["/home/dock_user/web/hpinetdb/hpinetbackend/src/introlog/hpinterolog.py", "--method", method, "--blastdb", "/home/dock_user/hpinetblast.db", "--ppidb", "ppidb", "--host_table", hspecies.toLowerCase(), "--pathogen_table", pspecies, "--host_identity", parseInt(identity), "--host_coverage", parseInt(coverage) ,"--host_evalue", parseFloat(evalue), "--pathogen_identity", parseInt(pi) ,"--pathogen_coverage", parseInt(pc) ,"--pathogen_evalue", parseFloat(pe) ,"--ppitables", intdb,  '--domdb', domdb,]);
-}
+  // Define the working directory
+  const cwd = '/opt/web/hpinetdb/hpinetbackend/src/introlog';
 
-getS.stdout.on('data', (data) => {
+  // Build the command arguments for the script
+  const commandArgs = [
+    "--method", method,
+    "--blastdb", "/home/dock_user/hpinetblast.db",
+    "--ppidb", "ppidb",
+    "--host_table", hspecies.toLowerCase(),
+    "--pathogen_table", pspecies,
+    "--host_identity", parseInt(identity),
+    "--host_coverage", parseInt(coverage),
+    "--host_evalue", parseFloat(evalue),
+    "--pathogen_identity", parseInt(pi),
+    "--pathogen_coverage", parseInt(pc),
+    "--pathogen_evalue", parseFloat(pe),
+    "--ppitables", intdb,
+    '--domdb', domdb,
+    '--id', idt
+  ];
 
+  if (genes.length > 0) {
+    commandArgs.push('--genes', genes);
+  }
+
+  const getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', ['hpinterolog.py', ...commandArgs], { cwd });
+
+  // Handle stdout data
+  getS.stdout.on('data', (data) => {
     output = data.toString();
-
     console.log('output was generated: ' + output);
-});
+  });
 
-getS.stdin.setEncoding = 'utf-8';
+  getS.stdin.setEncoding = 'utf-8';
 
-getS.stderr.on('data', (data) => {
-    
-    console.log('error:' + data);
-});
-return new Promise((res, rej) => {
+  // Handle stderr data
+  getS.stderr.on('data', (data) => {
+    console.log('error: ' + data);
+  });
 
+  return new Promise((resolve, reject) => {
     getS.stdout.on('end', async function (code) {
+      const resultData = output.split('\n');
+      console.log(resultData[0]);
+      resolve(resultData[0]);
+    });
+  });
+};
 
-    const rid = output.split('\n')
-    console.log(rid[0])
-    res(rid[0])
-    })
- });
-
-}
-
-module.exports = getPPI
-
-
+module.exports = getPPI;
