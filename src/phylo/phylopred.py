ded4a88e46355c45c8ebaa2050e6756230de9427
navduhan <duhan27dec@gmail.com>
2023-10-25 15:41:16 -0600

    update

--- src/phylo/phylopred.py
+++ src/phylo/phylopred.py
@@ -94,8 +94,8 @@ def run_blast(genomeNumber,poolFolder, poolList,host_fasta, pathogen_fasta, eval
         host_files.append(f"host/{host_fasta_out}_blast_{i}.txt")
         pathogen_files.append(f"pathogen/{pathogen_fasta_out}_blast_{i}.txt")
         # print(f"working on genome {i}")
-        cmd = f"diamond blastp --db {poolFolder}/{poolList[i]} -q {host_fasta} --evalue {evalue} --out host/{host_fasta_out}_blast_{i}.txt --outfmt 6 qseqid sseqid pident evalue bitscore qcovhsp -k 1 --threads 6"
-        pcmd =f"diamond blastp --db {poolFolder}/{poolList[i]} -q {pathogen_fasta} --evalue {pevalue} --out pathogen/{pathogen_fasta_out}_blast_{i}.txt --outfmt 6 qseqid sseqid pident evalue bitscore qcovhsp -k 1 --threads 6"
+        cmd = f"diamond blastp --db {poolFolder}/{poolList[i]} -q {host_fasta} --evalue {evalue} --out {host_fasta_out}_blast_{i}.txt --outfmt 6 qseqid sseqid pident evalue bitscore qcovhsp -k 1 --threads 6"
+        pcmd =f"diamond blastp --db {poolFolder}/{poolList[i]} -q {pathogen_fasta} --evalue {pevalue} --out {pathogen_fasta_out}_blast_{i}.txt --outfmt 6 qseqid sseqid pident evalue bitscore qcovhsp -k 1 --threads 6"
         with open(os.path.join(os.getcwd(), "host.out"), 'w+') as fout:
             with open(os.path.join(os.getcwd(), "host.err"), 'w+') as ferr:
                 job_id= subprocess.call(cmd, shell=True, stdout=fout,stderr=ferr)
