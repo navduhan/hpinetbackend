1528ddea888329de280dcf8efb0239c158c04f64
navduhan <duhan27dec@gmail.com>
2023-10-25 14:17:16 -0600

    update

--- src/gosemsim/goPPI.js
+++ src/gosemsim/goPPI.js
@@ -1,39 +1,76 @@
 const { spawn } = require('child_process');
-
 const path = require('path');
-const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, pathogen_genes)=>{
 
-const host_genes2 = host_genes.replace(" ",'')
-console.log(host_genes2)
-let output;
-let getS;
-console.log("/opt/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py", "--hgenes", host_genes2, "--pgenes", pathogen_genes, "--host", hspecies, "--pathogen", pspecies, "--method", method, "--score", score, "--t", threshold )
+// Define a function to run the Python script
+const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, pathogen_genes) => {
+  // Remove any spaces from host_genes
+  const host_genes2 = host_genes.replace(" ", '');
+  console.log(host_genes2);
 
-    getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', ["/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py", "--hgenes", host_genes2, "--pgenes", pathogen_genes, "--host", hspecies, "--pathogen", pspecies, "--method", method, "--score", score, "--t", threshold]);
+  // Initialize variables
+  let output;
+  let getS;
 
-getS.stdout.on('data', (data) => {
+  // Log the command that will be executed
+  console.log(
+    "/opt/miniconda3/envs/ml-gpu/bin/python3",
+    [
+      "/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py",
+      "--hgenes",
+      host_genes2,
+      "--pgenes",
+      pathogen_genes,
+      "--host",
+      hspecies,
+      "--pathogen",
+      pspecies,
+      "--method",
+      method,
+      "--score",
+      score,
+      "--t",
+      threshold
+    ].join(" ") // Combine command arguments into a string
+  );
 
+  // Spawn a Python process to run the script
+  getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', [
+    "/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py",
+    "--hgenes",
+    host_genes2,
+    "--pgenes",
+    pathogen_genes,
+    "--host",
+    hspecies,
+    "--pathogen",
+    pspecies,
+    "--method",
+    method,
+    "--score",
+    score,
+    "--t",
+    threshold
+  ]);
+
+  // Handle standard output from the Python process
+  getS.stdout.on('data', (data) => {
     output = data.toString();
-
     console.log('output was generated: ' + output);
-});
+  });
 
-getS.stdin.setEncoding = 'utf-8';
+  // Handle standard error from the Python process
+  getS.stderr.on('data', (data) => {
+    console.log('error: ' + data);
+  });
 
-getS.stderr.on('data', (data) => {
-    
-    console.log('error:' + data);
-});
-return new Promise((res, rej) => {
-
+  // Return a Promise to resolve when the process ends
+  return new Promise((res, rej) => {
     getS.stdout.on('end', async function (code) {
+      const rid = output;
+      console.log(rid);
+      res(rid[0]); // Assuming you want to resolve the first character of the output
+    });
+  });
+};
 
-    const rid = output.split('\n')
-    console.log(rid[0])
-    res(rid[0])
-    })
- });
-
-}
-
-module.exports = getGOPPI
+module.exports = getGOPPI;
