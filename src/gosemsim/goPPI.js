c9ebd520b691ca56d2a366f2f32e899ed74d64e7
navduhan <duhan27dec@gmail.com>
2023-10-25 14:50:20 -0600

    update

--- src/gosemsim/goPPI.js
+++ src/gosemsim/goPPI.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path');
 
 // Define a function to run the Python script
@@ -9,44 +9,24 @@ const getGOPPI = (method, hspecies, pspecies, score, threshold, host_genes, path
 
   // Initialize variables
   let output = '';
-  const commandArgs = [
-    "/opt/miniconda3/envs/ml-gpu/bin/python3",
-    "/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py",
-    "--hgenes", host_genes2,
-    "--pgenes", pathogen_genes,
-    "--host", hspecies,
-    "--pathogen", pspecies,
-    "--method", method,
-    "--score", score,
-    "--t", threshold
-  ];
+  const command = `"/opt/miniconda3/envs/ml-gpu/bin/python3" ` +
+    `"/home/dock_user/web/hpinetdb/hpinetbackend/src/gosemsim/goSemSim.py" ` +
+    `--hgenes ${host_genes2} ` +
+    `--pgenes ${pathogen_genes} ` +
+    `--host ${hspecies} ` +
+    `--pathogen ${pspecies} ` +
+    `--method ${method} ` +
+    `--score ${score} ` +
+    `--t ${threshold}`;
 
-  // Spawn a Python process to run the script
-  const getS = spawn(commandArgs[0], commandArgs.slice(1));
-  // Handle stdout data
-  const stdoutPromise = new Promise((resolve) => {
-    getS.stdout.on('data', (data) => {
-      output += data.toString();
-      console.log('output was generated: ' + data.toString());
-    });
-    getS.stdout.on('end', () => {
-      resolve();
-    });
-  });
+  try {
+    output = execSync(command).toString();
+    console.log('output was generated: ' + output);
+  } catch (error) {
+    console.error('Error: ' + error);
+  }
 
-  // Handle stderr data
-  const stderrPromise = new Promise((resolve) => {
-    getS.stderr.on('data', (data) => {
-      console.log('error: ' + data);
-    });
-    getS.stderr.on('end', () => {
-      resolve();
-    });
-  });
-
-  return Promise.all([stdoutPromise, stderrPromise]).then(() => {
-    return output;
-  });
+  return output;
 };
 
 module.exports = getGOPPI;
