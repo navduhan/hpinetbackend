b5c5e466f53868eaf40ca34643f10cbfa2b3f9ae
navduhan <duhan27dec@gmail.com>
2023-10-25 11:56:25 -0600

    rollback update

--- src/introlog/introlog.js
+++ src/introlog/introlog.js
@@ -1,14 +1,14 @@
 const { spawn } = require('child_process');
+const path = require('path');
 
 // Function to run the hpinterolog.py script
 const getPPI = async (method, hspecies, pspecies, identity, coverage, evalue, pi, pc, pe, intdb, domdb, genes, idt) => {
   let output;
 
-  // Define the working directory
-  const cwd = '/opt/web/hpinetdb/hpinetbackend/src/introlog';
-
   // Build the command arguments for the script
   const commandArgs = [
+    "/opt/miniconda3/envs/ml-gpu/bin/python3",
+    "/home/dock_user/web/hpinetdb/hpinetbackend/src/introlog/hpinterolog.py",
     "--method", method,
     "--blastdb", "/home/dock_user/hpinetblast.db",
     "--ppidb", "ppidb",
@@ -29,7 +29,7 @@ const getPPI = async (method, hspecies, pspecies, identity, coverage, evalue, pi
     commandArgs.push('--genes', genes);
   }
 
-  const getS = spawn('/opt/miniconda3/envs/ml-gpu/bin/python3', ['hpinterolog.py', ...commandArgs], { cwd });
+  const getS = spawn(commandArgs[0], commandArgs.slice(1));
 
   // Handle stdout data
   getS.stdout.on('data', (data) => {
