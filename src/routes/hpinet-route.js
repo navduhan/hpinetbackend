fd4de6f0f4174e6591e524b287e314b99b7ccb02
navduhan <duhan27dec@gmail.com>
2023-10-30 14:01:51 -0600

    update

--- src/routes/hpinet-route.js
+++ src/routes/hpinet-route.js
@@ -133,11 +133,14 @@ router.route('/results/').get(async (req, res) => {
 
 router.route('/annotation/').get(async(req,res) =>{
 
+console.log(req.query)
+
 let {host, pathogen, hid, pid} =req.query
+console.log(hid, pid)
 
 let hgo_results = await GO['host'].find({'species': host.toLowerCase() , 'gene':hid})
 let pgo_results = await GO['pathogen'].find({'species': pathogen, 'gene':pid})
-let hkegg_results = await KEGG['host'].find({'species': host , 'gene':hid})
+let hkegg_results = await KEGG['host'].find({'species': host, 'gene':hid})
 let pkegg_results = await KEGG['pathogen'].find({'species': pathogen , 'gene':pid})
 let hlocal_results = await Local['host'].find({'species': host , 'gene':hid})
 let plocal_results = await Local['pathogen'].find({'species': pathogen, 'gene':pid})
