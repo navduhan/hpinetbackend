bee6b0cb6adef9e882fede557d967d15ccec13e4
navduhan <duhan27dec@gmail.com>
2023-11-05 02:21:25 -0700

    update

--- src/routes/hpinet-route.js
+++ src/routes/hpinet-route.js
@@ -133,7 +133,7 @@ router.route('/ppi').post(async (req, res) => {
     } 
   });
 
-  let results = await getPPI(body.category, body.hspecies, body.pspecies, body.hi, body.hc, body.he, body.pi, body.pc, body.pe, body.intdb, body.domdb, isgenes, species)
+  let results = await getPPI(body.category, body.hspecies, body.pspecies, body.hi, body.hc, body.he, body.pi, body.pc, body.pe, body.intdb, body.domdb, isgenes,body.ids)
   
   res.json(results)
 
