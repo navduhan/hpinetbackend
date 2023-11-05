61ece9ff945399ce04fa7398cd7370449d5d8b17
navduhan <duhan27dec@gmail.com>
2023-11-04 21:42:24 -0600

    added go

--- src/routes/hpinet-route.js
+++ src/routes/hpinet-route.js
@@ -70,6 +70,10 @@ router.route('/ppi').post(async (req, res) => {
 
   const body = JSON.parse(JSON.stringify(req.body));
 
+  if (body.searchTyep ==='keyword'){
+    console.log(body.keyword)
+  }
+
   let results = await getPPI(body.category, body.hspecies, body.pspecies, body.hi, body.hc, body.he, body.pi, body.pc, body.pe, body.intdb, body.domdb, body.genes, body.ids)
   
   res.json(results)
